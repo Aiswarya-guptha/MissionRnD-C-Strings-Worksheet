48462669ba04ba6fc6644df88b0978517b030213
aiswarya-guptha <aiswaryaguptha36@gmail.com>
2015-11-26 19:18:57 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -12,6 +12,23 @@ NOTES: Don't create new string.
 #include <Stdio.h>
 #include <string.h>
 
+void swapWord(char *input, int from, int till){
+	while (from < till){
+		input[from] = input[from] + input[till];
+		input[till] = input[from] - input[till];
+		input[from] = input[from] - input[till];
+		from++;
+		till--;
+	}
+}
+
 void str_words_in_rev(char *input, int len){
 	
+	for (int from = 0, till = 0; till < len; till++){
+		if (till == (len - 1) || input[till + 1] == ' '){
+			swapWord(input, from, till);
+			from = till+2;
+		}
+	}
+	swapWord(input, 0, (len - 1));
 }
